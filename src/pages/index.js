78f527531953a211362b35dd944b643800ee41c1
Sana Sreeraj <iamsanasreeraj@gmail.com>
2024-02-01 22:56:01 +0530

    Navbar

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Hero from '@/components/hero/hero'
+import Nav from '@/components/nav/nav'
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+        <Nav/>
+        <Hero/>
+    </div>
+    
+  )
+}
+
+export default index
