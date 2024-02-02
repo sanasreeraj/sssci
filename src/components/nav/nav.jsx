a369dfb63d7876ca673e750d5ba71c6a3d109d15
Sana Sreeraj <iamsanasreeraj@gmail.com>
2024-02-02 18:27:32 +0530

    Updated Navbar

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,31 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const Nav = () => {
+  const [state, setState] = useState(false);
+  const router = useRouter();
+
+
   return (
     <div>
-        <header className="text-gray-600 body-font">
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-yellow-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-      <span className="ml-3 text-3xl font-bold">SSSCI</span>
-    </a>
-    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-gray-900">Products</a>
-      <a className="mr-5 hover:text-gray-900">Outlets</a>
-      <a className="mr-5 hover:text-gray-900">About</a>
-      <a className="mr-8 hover:text-gray-900">Contact Us</a>
-    </nav>
-    <button className="inline-flex items-center mr-24 bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
-    </button>
-  </div>
-</header>
+      <nav className="bg-[#F9EFDB]  w-full md:static md:text-sm md:border-none">
+        <div className="items-center px-4 max-w-screen-xl mx-auto md:flex ">
+          <div className="flex items-center justify-between h-16  md:block">
+            <a href="#" className='flex items-center'>
+            <div className="md:hidden">
+              <button
+                className="text-gray-500 hover:text-gray-800 mt-2 pr-2"
+                onClick={() => setState(!state)}
+              >
+                {state ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 mr-2"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+            </a>
+            
+          </div>
+          <div
+            className={`flex-1 pb-3 mt-2 border-gray 400 border-t md:border-0 md:block md:pb-0 md:mt-0 ${state ? 'block' : 'hidden'
+              }`}
+          >
+            <ul className=" flex justify-center flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6">
+              <NavLink className="text-lg" path="/" label="Home" currentPath={router.pathname} />
+              <NavLink className="text-lg" path="/about" label="About" currentPath={router.pathname} />
+              <NavLink className="text-lg" path="/products" label="Products" currentPath={router.pathname} />
+              <NavLink className="text-lg" path="/contact" label="Contact" currentPath={router.pathname} />
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+const NavLink = ({ path, label, currentPath, className }) => (
+  <li>
+    <Link href={path}>
+      <p className={`text-gray-700 text-sm font-semibold ${currentPath === path ? 'border-b-2 border-blue-500' : ''} ${className || ''}`}>
+        {label}
+      </p>
+    </Link>
+  </li>
+);
+
+export default Nav;
---
+++ src/pages/products.js
@@ -0,0 +1,13 @@
+import Products from '@/components/products/products'
+import React from 'react'
+
+
+const products = () => {
+  return (
+    <div>
+      <Products/>
+    </div>
+  )
+}
+
+export default products
