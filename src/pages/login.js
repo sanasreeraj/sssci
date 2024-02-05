9864f3a70a9e9173443d290558250429b911a42f
Sana Sreeraj <iamsanasreeraj@gmail.com>
2024-02-05 23:26:56 +0530

    Login Page file added

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import Login from '@/components/login/login'
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+        <Login/>
+    </div>
+  )
+}
+
+export default login
