75671beea542c57e8ee1879eff8019f6d170d3d0
Sana Sreeraj <iamsanasreeraj@gmail.com>
2024-02-07 00:58:12 +0530

    Login Code update1

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,120 +1,76 @@
-import React from "react";
+import Head from 'next/head';
 
 const Login = () => {
   return (
-    <div>
-      <div className="bg-[#F9EFDB] min-h-screen flex items-center justify-center">
-        <div className="bg-[#F9EFDB] p-8 rounded-lg shadow-md w-full max-w-md">
+    <div className="flex justify-center items-center h-screen bg-[#F9EFDB]">
+      <Head>
+        <title>Login</title>
+      </Head>
+      <div className="flex w-full max-w-3xl">
+        <div className="hidden md:block md:w-1/2 pr-16">
+          {/* Left half for photo */}
+          {/* Adjusted width and added max height */}
           <img
-            src="/company-logo.png"
-            alt="Company Logo"
-            className="mx-auto mb-4"
+            src="/logo.png"
+            alt="Your Photo"
+            className="object-cover w-full h-auto rounded-l-lg max-h-full"
           />
-
-          {/* Form */}
-          <form>
-            {/* Form fields */}
-            <div className="mb-4">
-              <label
-                htmlFor="firstName"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="firstName"
-                name="firstName"
-                className="mt-1 p-2 w-full border rounded-md"
-              />
-            </div>
-
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="mt-1 p-2 w-full border rounded-md"
-              />
-            </div>
-
-            <div className="mb-4">
-              <label
-                htmlFor="phoneNumber"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Phone Number
-              </label>
-              <input
-                type="tel"
-                id="phoneNumber"
-                name="phoneNumber"
-                className="mt-1 p-2 w-full border rounded-md"
-              />
-            </div>
-
-            <div className="mb-4">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                className="mt-1 p-2 w-full border rounded-md"
-              />
-            </div>
-
-            <div className="mb-4">
-              <label
-                htmlFor="confirmPassword"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Confirm Password
-              </label>
-              <input
-                type="password"
-                id="confirmPassword"
-                name="confirmPassword"
-                className="mt-1 p-2 w-full border rounded-md"
-              />
-            </div>
-
-            {/* OR Option */}
-            <div className="mb-4">
-              <div className="flex items-center">
-                <div className="border-b border-gray-300 flex-grow mr-4"></div>
-                <span className="text-sm font-medium text-gray-600">
-                  OR
-                </span>
-                <div className="border-b border-gray-300 flex-grow ml-4"></div>
+        </div>
+        <div className="md:w-1/2">
+          {/* Right half for login box */}
+          <div className="bg-white rounded-lg p-8 shadow-md">
+            <h1 className="text-2xl font-bold mb-4">Login</h1>
+            <div className="relative mb-4">
+              <div className="border rounded-lg px-4 py-2 w-full focus:outline-none focus:border-blue-500">
+                <input
+                  type="email"
+                  className="w-full h-full focus:outline-none"
+                  id="identifierId"
+                  name="identifier"
+                  autoComplete="username"
+                  spellCheck="false"
+                  tabIndex="0"
+                  aria-label="Email or phone"
+                  aria-disabled="false"
+                  autoCapitalize="none"
+                  placeholder="Email or phone"
+                />
               </div>
             </div>
-
-            {/* Submit button */}
-            <button
-              type="submit"
-              className="w-full mb-4  bg-blue-300 m text-white p-2 rounded-md hover:bg-blue-600"
-            >
+            <div className="relative mb-4">
+              <div className="border rounded-lg px-4 py-2 w-full focus:outline-none focus:border-blue-500">
+                <input
+                  type="password"
+                  className="w-full h-full focus:outline-none"
+                  id="password"
+                  name="password"
+                  autoComplete="current-password"
+                  spellCheck="false"
+                  tabIndex="0"
+                  aria-label="Password"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <button className="bg-blue-500 text-white rounded-lg px-4 py-2 mb-4 w-full hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+              Login
+            </button>
+            <div className="flex items-center justify-between">
+              <hr className="flex-grow mr-4" />
+              <span className="text-gray-500">Or</span>
+              <hr className="flex-grow ml-4" />
+            </div>
+            <button className="bg-red-500 text-white rounded-lg px-4 py-2 mt-4 w-full hover:bg-red-600 focus:outline-none focus:bg-red-600">
               Continue with Google
+              <span className="ml-2">
+                <img
+                  src="/google-logo.png"
+                  alt="Google Logo"
+                  className="w-4 h-4 inline-block"
+                />
+              </span>
             </button>
-
-            <button
-              type="submit"
-              className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
-            >
-              Sign Up
-            </button>
-          </form>
+          </div>
         </div>
       </div>
     </div>
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,121 +1,79 @@
-import React, { useState } from "react";
-import { useRouter } from "next/router";
-import Link from "next/link";
+import Head from "next/head";
 
-const Nav = () => {
-  const [state, setState] = useState(false);
-  const router = useRouter();
-
+const Login = () => {
   return (
-    <div>
-      <nav className="bg-[#F9EFDB]  w-full md:static md:text-sm md:border-none">
-        <div className="items-center px-4 max-w-screen-xl mx-auto md:flex ">
-          <div className="flex items-center justify-between h-16  md:block">
-            <a href="#" className="flex items-center">
-              <div className="md:hidden">
-                <button
-                  className="text-gray-500 hover:text-gray-800 mt-2 pr-2"
-                  onClick={() => setState(!state)}
-                >
-                  {state ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6 mr-2"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                      />
-                    </svg>
-                  )}
-                </button>
+    <div className="flex justify-center items-center min-h-screen bg-[#F9EFDB]">
+      <Head>
+        <title>Login</title>
+      </Head>
+      <div className="flex flex-col md:flex-row w-full h-full max-w-6xl mx-auto">
+        <div className="md:w-1/2 flex justify-center items-center">
+          {/* Left half for photo */}
+          <img
+            src="/logo.png"
+            alt="Your Photo"
+            className="object-cover h-64 md:h-auto max-w-full rounded-l-lg"
+          />
+        </div>
+        <div className="md:w-1/2 flex justify-center items-center">
+          {/* Right half for login box */}
+          <div className="bg-white rounded-lg p-8 shadow-md">
+            <h1 className="text-2xl font-bold mb-4">Login</h1>
+            <div className="relative mb-4">
+              <div className="border rounded-lg px-4 py-2 w-full focus:outline-none focus:border-blue-500">
+                <input
+                  type="email"
+                  className="w-full h-full focus:outline-none"
+                  id="identifierId"
+                  name="identifier"
+                  autoComplete="username"
+                  spellCheck="false"
+                  tabIndex="0"
+                  aria-label="Email or phone"
+                  aria-disabled="false"
+                  autoCapitalize="none"
+                  placeholder="Email or phone"
+                />
               </div>
-            </a>
-          </div>
-          <div
-            className={`flex-1 pb-3 mt-2 border-gray 400 border-t md:border-0 md:block md:pb-0 md:mt-0 ${
-              state ? "block" : "hidden"
-            }`}
-          >
-            <ul className=" flex justify-center flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6">
-              <NavLink
-                className="text-lg"
-                path="/"
-                label="Home"
-                currentPath={router.pathname}
-              />
-              <NavLink
-                className="text-lg"
-                path="/about"
-                label="About"
-                currentPath={router.pathname}
-              />
-              <NavLink
-                className="text-lg"
-                path="/products"
-                label="Products"
-                currentPath={router.pathname}
-              />
-              <NavLink
-                className="text-lg"
-                path="/contact"
-                label="contact"
-                currentPath={router.pathname}
-              />
-            </ul>
+            </div>
+            <div className="relative mb-4">
+              <div className="border rounded-lg px-4 py-2 w-full focus:outline-none focus:border-blue-500">
+                <input
+                  type="password"
+                  className="w-full h-full focus:outline-none"
+                  id="password"
+                  name="password"
+                  autoComplete="current-password"
+                  spellCheck="false"
+                  tabIndex="0"
+                  aria-label="Password"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <button className="bg-blue-500 text-white rounded-lg px-4 py-2 mb-4 w-full hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+              Login
+            </button>
+            <div className="flex items-center justify-between">
+              <hr className="flex-grow mr-4" />
+              <span className="text-gray-500">Or</span>
+              <hr className="flex-grow ml-4" />
+            </div>
+            <button className="bg-red-500 text-white rounded-lg px-4 py-2 mt-4 w-full hover:bg-red-600 focus:outline-none focus:bg-red-600">
+              Continue with Google
+              <span className="ml-2">
+                <img
+                  src="/google-logo.png"
+                  alt="Google Logo"
+                  className="w-4 h-4 inline-block"
+                />
+              </span>
+            </button>
           </div>
         </div>
-        <button className="flex items-center bg-[#F9EFDB] border-black border rounded-full px-4 py-2">
-          <span className="text-black mr-2">Sign In</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            viewBox="0 0 20 20"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            {/* SVG path for the arrow icon */}
-            <path d="M5 10l7-7m0 14l7-7" />
-          </svg>
-        </button>
-      </nav>
+      </div>
     </div>
   );
 };
 
-const NavLink = ({ path, label, currentPath, className }) => (
-  <li>
-    <Link href={path}>
-      <p
-        className={`text-gray-700 text-sm font-semibold ${
-          currentPath === path ? "border-b-2 border-blue-500" : ""
-        } ${className || ""}`}
-      >
-        {label}
-      </p>
-    </Link>
-  </li>
-);
-
-export default Nav;
+export default Login;
